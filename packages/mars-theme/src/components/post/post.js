31a20d5412f26066f395712eb99200d658c13b57
chaycesol <chayce.solchaga@gmail.com>
2020-10-20 14:39:31 -0700

    additions to blog

--- packages/mars-theme/src/components/post/post.js
+++ packages/mars-theme/src/components/post/post.js
@@ -2,146 +2,164 @@ import React, { useEffect } from "react";
 import { connect, styled } from "frontity";
 import Link from "../link";
 import List from "../list";
+import FeaturedMedia from "../featured-media";
 
-import { getUrlData, formatPostData } from "../../helpers";
-import PostEntryInfo from "./postEntryInfo";
-import Taxonomies from "./taxonomies";
-import PostEntryMedia from "./postEntryMedia";
-import featuredStyles from "../styles/featuredStyles";
-
-const Post = ({ state, libraries, actions }) => {
-  const data = getUrlData(state);
+const Post = ({ state, actions, libraries }) => {
+  // Get information about the current URL.
+  const data = state.source.get(state.router.link);
+  // Get the data of the post.
   const post = state.source[data.type][data.id];
+  // Get the data of the author.
+  const author = state.source.author[post.author];
+  // Get a human readable date.
+  const date = new Date(post.date);
 
-  const { postMeta, postInfo, featured } = state.theme;
+  // Get the html2react component.
   const Html2React = libraries.html2react.Component;
 
-  const {
-    author,
-    publishDate,
-    title,
-    categories,
-    tags,
-    featured_media,
-    content
-  } = formatPostData(state, post);
-  const date = new Date(publishDate);
   /**
    * Once the post has loaded in the DOM, prefetch both the
    * home posts and the list component so if the user visits
    * the home page, everything is ready and it loads instantly.
    */
-
   useEffect(() => {
     actions.source.fetch("/");
     List.preload();
   }, []);
 
+  // Load the post, but only if the data is ready.
   return data.isReady ? (
-    <Container className="container" sx={{ maxWidth: "l" }}>
-      <article
-        sx={{ variant: "card.default", "div figure": { maxWidth: 804, mx: 0 } }}
-      >
-        {featured.showOnPost && (
-          <PostEntryMedia
-            id={featured_media.id}
-            ratio={16 / 9}
-            sx={{
-              ...featuredStyles
-            }}
-          />
-        )}
-        <Box>
-          <h1
-            dangerouslySetInnerHTML={{ __html: title }}
-            sx={{
-              textTransform: "uppercase",
-              fontSize: "xl"
-            }}
-          />
-          {data.isPost && postInfo.showOnPost && (
-            <PostEntryInfo author={author} date={date} />
-          )}
+    <ArticleContainer>
+      <div className="post-title">
+        <Title dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
 
-          <Html2React html={content} />
-        </Box>
-
-        {data.isPost && postMeta.showOnPost && (
-          <div className="postEntryMeta">
-            <Taxonomies tax={categories} name="Categories" />
-            <Taxonomies tax={tags} name="Tags" />
+        {/* Only display author and date on posts */}
+        {data.isPost && (
+          <div>
+            {author && (
+              <StyledLink link={author.link}>
+                <Author>
+                  By <b>{author.name}</b>
+                </Author>
+              </StyledLink>
+            )}
+            <DateWrapper>
+              {" "}
+              on <b>{date.toDateString()}</b>
+            </DateWrapper>
           </div>
         )}
-      </article>
-    </Container>
+      </div>
+
+      {/* Look at the settings to see if we should include the featured image */}
+      {state.theme.featured.showOnPost && (
+        <FeaturedMedia id={post.featured_media} />
+      )}
+
+      <Content>
+        <Html2React html={post.content.rendered} />
+      </Content>
+    </ArticleContainer>
   ) : null;
 };
 
 export default connect(Post);
 
-const Container = styled.div`
-  font-family: 'Montserrat', sans-serif;
-  width: 100%;
-  margin: 0;
-`
+const ArticleContainer = styled.div`
+  width:100%;
+  max-width:1035px;
+  margin: 0 auto;
+  padding-right: 15px;
+  padding-left: 15px; 
+  .post-title {
+    text-align:center;    
+  }
+`;
 
-const Box = styled.div`
-width: 
-`
+const Title = styled.h1`
+  margin-bottom: 1.2rem;
+`;
 
+const StyledLink = styled(Link)`
+  padding: 15px 0;
+`;
 
+const Author = styled.p`
+  color: rgba(12, 17, 43, 0.9);
+  font-size: 0.9em;
+  display: inline;
+`;
+
+const DateWrapper = styled.p`
+  color: rgba(12, 17, 43, 0.9);
+  font-size: 0.9em;
+  display: inline;
+`;
 
 /**
  * This component is the parent of the `content.rendered` HTML. We can use nested
  * selectors to style that HTML.
  */
 const Content = styled.div`
-  color: rgba(12, 17, 43, 0.8);
   word-break: break-word;
-
   * {
-    max-width: 100%;
+    max-width: 771px;
+    width: 100%;
+    margin:0 auto;
   }
-
   p {
-    line-height: 1.6em;
+    margin-bottom:1.5rem;
   }
-
   img {
     width: 100%;
     object-fit: cover;
     object-position: center;
   }
-
   figure {
     margin: 24px auto;
     /* next line overrides an inline style of the figure element. */
-    width: 100% !important;
-
+    // width: 100% !important;
     figcaption {
       font-size: 0.7em;
     }
   }
-
   iframe {
     display: block;
-    margin: auto;
+    margin: 0 auto;
   }
-
   blockquote {
-    margin: 16px 0;
+    margin: 0 auto;
     background-color: rgba(0, 0, 0, 0.1);
     border-left: 4px solid rgba(12, 17, 43);
     padding: 4px 16px;
   }
-
+  .wp-block-embed {
+    max-width: 100%;
+    position: relative;
+    width: 100%;
+    margin: 1.5rem auto;
+    .wp-block-embed__wrapper {
+      &::before {
+        content: "";
+        display: block;
+        padding-top: 56.25%;
+      }
+    }
+    iframe {
+      max-width: 100%;
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      border: 0;
+    }
+  }
   a {
     color: rgb(31, 56, 197);
     text-decoration: underline;
   }
-
   /* Input fields styles */
-
   input[type="text"],
   input[type="email"],
   input[type="url"],
@@ -156,19 +174,17 @@ const Content = styled.div`
     font-weight: 400;
     line-height: 1.5;
     color: #495057;
-    background-color: #f6f2ec;
+    background-color:var(--white);
     background-clip: padding-box;
     border: 1px solid #ced4da;
     border-radius: 4px;
     outline-color: transparent;
     transition: outline-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
     margin: 8px 0 4px 0;
-
     &:focus {
       outline-color: #1f38c5;
     }
   }
-
   input[type="submit"] {
     display: inline-block;
     margin-bottom: 0;
@@ -185,30 +201,25 @@ const Content = styled.div`
     font-size: 14px;
     line-height: 1.42857143;
     border-radius: 4px;
-    color: #fff;
-    background-color: #1f38c5;
+    color: var(--white);
+    background-color:var(--brand);
   }
-
   /* WordPress Core Align Classes */
-
   @media (min-width: 420px) {
     img.aligncenter,
     img.alignleft,
     img.alignright {
       width: auto;
     }
-
     .aligncenter {
       display: block;
       margin-left: auto;
       margin-right: auto;
     }
-
     .alignright {
       float: right;
       margin-left: 24px;
     }
-
     .alignleft {
       float: left;
       margin-right: 24px;
